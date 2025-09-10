da52e8de68462f583fd98de73c85f5748ea550a0
Mayuri Wankhede <mayuri05wankhede@gmail.com>
2025-09-10 16:40:41 +0530

    Jenkin first commit

---
+++ JenkinDemo/src/test/java/GIT/JenkinDemo/LoginPractice.java
@@ -0,0 +1,40 @@
+package GIT.JenkinDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginPractice {
+	
+	@Test
+	public void login() throws InterruptedException
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		// driver.manage().window().maximize();
+		
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		
+		WebElement uName=driver.findElement(By.cssSelector("#inputUsername"));		
+		uName.sendKeys("megha"); 
+		
+		WebElement pwd= driver.findElement(By.name("inputPassword"));
+		pwd.sendKeys("password");
+		
+		WebElement btn = driver.findElement(By.cssSelector(".submit.signInBtn"));
+		btn.submit();// submits the form/ click
+		
+		Thread.sleep(2000);
+		System.out.println(driver.findElement(By.className("error")).getText());
+
+		System.out.println(driver.findElement(By.tagName("a")).getText());
+		driver.close();		
+	}
+
+	
+
+}
